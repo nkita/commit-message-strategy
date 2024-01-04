247a8e2a4df2d247347183b1cfb35acf5fce59de
nkita <nkitam7@gmail.com>
2024-01-04 18:08:07 +0900

    feat: add a link button to cc100 on the Top page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Image from "next/image"
+import Link from "next/link";
 import { Header } from '@/components/header';
 import { Card } from '@/components/';
 
@@ -9,16 +10,21 @@ export default function Home() {
   return (
     <>
       <Header isBorder={false} />
-      <div className="fixed top-13 left-1/3 text-9xl text-blue-300 blur-3xl whitespace-nowrap">
-        Message Recipe
-      </div>
       <section className="pt-28 px-8 pb-28">
+        <div className="absolute top-13 right-1/3 text-9xl text-blue-300 blur-3xl whitespace-nowrap">
+          Message Recipe
+        </div>
         <h1 className="text-center text-3xl pb-3 font-extrabold">複雑なコミットメッセージを簡単に美しく。</h1>
         <h1 className="text-center text-3xl font-extrabold">あなたのためのコミットメッセージ作成をサポートします。</h1>
         <div className="flex justify-center items-center pt-28">
           <div className="border rounded-lg shadow-md">
             <Image src="/image.png" alt="layout" width={2226 * 0.35} height={1202 * 0.35} ></Image>
           </div>
+        </div>
+        <div className="flex justify-center pt-28">
+          <Link href="cc100" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 shadow-md border border-gray-100 hover:bg-gradient-to-bl hover:border-blue-300 hover:ring-4 hover:ring-blue-100 hover:cursor-pointer font-medium rounded-xl text-xl px-5 py-2.5 ease-in duration-200">
+            Conventional Commits 1.0.0で試してみる
+          </Link>
         </div>
       </section>
 
@@ -31,7 +37,7 @@ export default function Home() {
 
       <section className="flex py-28 items-center flex-col justify-center">
         <h1 className="font-bold text-xl py-4 px-8 ">メッセージを書く</h1>
-        <section className="p-8 bg-gray-50 rounded-lg border overflow-y-auto overflow-x-hidden h-[400px] w-[500px]  md:h-[450px] md:w-[762px] md:overflow-x-auto md:overflow-y-hidden">
+        <section className="p-8 bg-gray-50 rounded-lg border overflow-y-auto overflow-x-hidden h-[400px] w-[500px]  md:h-[450px] md:w-10/12 md:overflow-x-auto md:overflow-y-hidden">
           <div className="grid grid-cols-1 gap-8 items-center md:grid-cols-none md:flex ">
             <Card
               emoji="🤗"
