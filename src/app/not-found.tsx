94e8a3af0083d1486ecd442313cee8cf987fd4d6
nkita <nkitam7@gmail.com>
2024-01-10 22:14:54 +0900

    fix: modify the not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,11 +9,13 @@ export default function Home() {
     return (
         <>
             <Header isBorder={false} />
-            <div className='flex justify-center content-center items-center h-[calc(100vh-100px)]'>
-                <h1 className='text-2xl'>
-                    This page could not be found.
-                </h1>
-                <span></span>
+            <div className='flex justify-center content-center items-center h-[calc(100vh-100px)] text-gray-600'>
+                <div className='text-center'>
+                    <h1 className='text-2xl'>
+                        <span className='font-bold '>404</span> | This page could not be found.
+                    </h1>
+                    <span className='leading-10 text-xl'>ページが見つかりません。</span>
+                </div>
             </div>
             <Footer addClass={"fixed bottom-0 w-screen bg-white/30 text-gray-500 text-sm backdrop-blur-md"} />
         </>
